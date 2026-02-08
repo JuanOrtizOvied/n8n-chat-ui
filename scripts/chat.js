f088f34a843a6098703832c8570bc1655406f541
JuanOrtizOvied <juan.ortiz.oviedo@gmail.com>
2026-02-08 15:00:00 -0500

    feat: add reset chat with history saved

--- scripts/chat.js
+++ scripts/chat.js
@@ -4,8 +4,8 @@
 
 import { CHAT_CONFIG, initializeSalesSession } from './config.js';
 import { state, setState } from './state.js';
-import { hideHeader } from './components/header.js';
-import { hideInfoCard } from './components/infoCard.js';
+import { hideHeader, showHeader } from './components/header.js';
+import { hideInfoCard, showInfoCard } from './components/infoCard.js';
 
 // Setup chat callbacks
 window.myChatCallbacks = {
@@ -33,6 +33,31 @@ window.myChatCallbacks = {
 
   onChatClear: function (data) {
     console.log("Chat cleared:", data);
+    const category = localStorage.getItem("sabbi_chat_selected_category");
+    const chat = localStorage.getItem(`${category}-session_CHAT`);
+
+    console.log("Chat data -session_CHAT:", chat);
+
+    // Remove selected category
+    localStorage.removeItem("sabbi_chat_selected_category");
+    
+    // Reset state
+    setState({ selectedCategory: null });
+
+    // Hide chat container
+    const chatContainer = document.getElementById('chatContainer');
+    chatContainer.classList.remove('active');
+    
+    // Show header and info card
+    setTimeout(() => {
+      showHeader();
+      showInfoCard();
+    }, 100);
+
+    // Show header and info card
+    setTimeout(() => {
+      localStorage.setItem(`${category}-session_CHAT`, chat);
+    }, 250);
   },
 
   onWidgetClose: function (data) {
@@ -41,25 +66,26 @@ window.myChatCallbacks = {
 };
 
 export async function initializeChat(category) {
-  if (state.chatInitialized) return;
-
   try {
     const module = await import('https://cdn.n8nchatui.com/v1/embed.js');
     const Chatbot = module.default;
-    
-    Chatbot.initFull({
-      n8nChatUrl: CHAT_CONFIG.n8nChatUrl,
-      callbackRegistry: "myChatCallbacks",
-      metadata: {
-        n8nchatui: {
-          sessionKey: `${category}-session`,
+
+    if (!state.chatInitialized) {
+      Chatbot.initFull({
+        n8nChatUrl: CHAT_CONFIG.n8nChatUrl,
+        callbackRegistry: "myChatCallbacks",
+        metadata: {
+          n8nchatui: {
+            sessionKey: `${category}-session`,
+          },
+          category: category,
+          pageType: "help",
+          user_uuid: state.userUuid,
         },
-        category: category,
-        pageType: "help",
-        user_uuid: state.userUuid,
-      },
-      theme: CHAT_CONFIG.theme,
-    });
+        theme: CHAT_CONFIG.theme,
+      });
+    };
+    
     
     setState({ chatInitialized: true });
     
